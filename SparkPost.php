5bd79dc708d3ce4a7a3fe6a5627aac64ec2fcd8b
Derick Alangi <alangiderick@gmail.com>
2019-01-07 09:09:27 +0100

    Fix typo in the text passed to die()
    Should now be 1.31+ as extension requires from there
    and above.
    
    Change-Id: I8d8ecd0a8171f7a17253bc0d88de831a724eeff1

--- SparkPost.php
+++ SparkPost.php
@@ -11,5 +11,5 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	);
 	return true;
 } else {
-	die( 'This version of the SparkPost extension requires MediaWiki 1.33+' );
+	die( 'This version of the SparkPost extension requires MediaWiki 1.31+' );
 }
