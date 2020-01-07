88951ba3c7b0f857e2efb07b766f7fc95372b6c4
Umherirrender <umherirrender_de.wp@web.de>
2020-01-07 18:56:55 +0100

    Fix phan config to make pass
    Change-Id: I610e44b63d11c94f0d4b5658c4e84381fea8f288

--- .phan/config.php
+++ .phan/config.php
@@ -2,4 +2,10 @@
 
 $cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
 
+// needs composer packages not part of mediawiki/vendor
+$cfg['directory_list'][] = 'vendor/sparkpost/';
+$cfg['directory_list'][] = 'vendor/guzzlehttp/';
+$cfg['directory_list'][] = 'vendor/php-http/';
+$cfg['exclude_analysis_directory_list'][] = 'vendor/';
+
 return $cfg;
